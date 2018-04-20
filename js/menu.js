d9c252727cbd7330966cbaedcaa94e0d35ad496a
sanfabiian <sanfabiian@gmail.com>
2018-04-20 16:10:05 -0500

    menu completo contenido a mitad

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+$(document).ready(main);
+var contador = 1;
+function main(){
+	$('.bt-menu').click(function(){
+		if(contador == 1){
+			$('header').animate({
+				left: '0'
+			});
+			contador = 0;
+		}else{
+			contador = 1;
+			$('header').animate({
+				left: '-100%'
+			});
+		}
+	});
+	$('.submenu').click(function(){
+		$(this).children('.menu-down').slideToggle();
+	});
+};
+
+
